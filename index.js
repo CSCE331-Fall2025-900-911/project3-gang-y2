afcdc9854586a6794e7559981f3a67a08af26112
DylanMMyers <135419290+DylanMMyers@users.noreply.github.com>
2025-10-29 19:35:12 -0500

    added port listener

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const port = process.env.port || 3000;
+app.listen(port, '0.0.0.0');
 
 app.get('/', (req, res) => {
   res.send('Basic template for our POS');
