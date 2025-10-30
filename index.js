8473c23cf86b6457a81bc8a1da0b3ccedea0bd06
DylanMMyers <135419290+DylanMMyers@users.noreply.github.com>
2025-10-29 19:39:23 -0500

    no more hardcode

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const app = express();
-const port = process.env.port || 3000;
-app.listen(port, '0.0.0.0');
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, '0.0.0.0');
 
 app.get('/', (req, res) => {
   res.send('Basic template for our POS');
 });
 
-app.listen(port, () => {
-    console.log(`listening on port ${port}`);
+app.listen(PORT, () => {
+    console.log(`listening on PORT ${PORT}`);
 });
