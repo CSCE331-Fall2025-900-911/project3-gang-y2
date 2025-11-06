faf0f08ef4a62e757ae12df500d168b1dcfe0cc8
“KaileeCho” <kcboss102@tamu.edu>
2025-11-05 19:12:43 -0600

    starting landing page and login

---
+++ myapp/frontend/src/App.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="homepage">
+      <nav className="navbar">
+        <div className="nav-container">
+          <h1 className="logo">MatchaBoba POS</h1>
+          <ul className="nav-links">
+            <li><Link to="/login">Login</Link></li>
+            <li><Link to="/kiosk">Customer Kiosk</Link></li>
+            <li><Link to="/menu">Menu Board</Link></li>
+          </ul>
+        </div>
+      </nav>
+
+      <main className="content">
+        <h2>Welcome to MatchaBoba POS</h2>
+        <p>
+          Tap in with us by logging in if you are an employee. Take a look at
+          our menu board! Or visit our self-service kiosk to order it yourself.
+        </p>
+      </main>
+    </div>
+  );
+}
+
+export default App;
