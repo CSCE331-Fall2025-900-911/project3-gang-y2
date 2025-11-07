22f17504b58862a82aa9afd3c72eb09cd3038382
MgSmith04 <MgSmith04@outlook.com>
2025-11-07 11:56:00 -0600

    added kiosk file & linked it to main, currently empty

--- myapp/frontend/src/main.jsx
+++ myapp/frontend/src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App.jsx";
 import Login from "./Login.jsx";
+import Kiosk from "./Kiosk.jsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
@@ -11,6 +12,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/kiosk" element={<Kiosk />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
