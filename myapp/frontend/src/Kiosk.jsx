076c18f996c945b7934c35199507860e30b69fa0
MgSmith04 <MgSmith04@outlook.com>
2025-11-09 16:13:02 -0600

    added menu item buttons to kiosk screen, need to add DB connections & order list

--- myapp/frontend/src/Kiosk.jsx
+++ myapp/frontend/src/Kiosk.jsx
@@ -1,30 +1,61 @@
 import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import { Link } from "react-router-dom";
 import "./Kiosk.css";
 
-// menuItems = {
-//     // call the backend here to get the menu items for the buttons?
-// };
+    const menuItems = [
+        'Classic Pearl Milk Black Tea', 'Classic Pearl Milk Green Tea', 'Classic Pearl Milk Oolong Tea', 
+        'Honey Pearl Milk Black Tea', 'Honey Pearl Milk Green Tea', 'Honey Pearl Milk Oolong Tea', 
+        'Coffee Creama', 'Coffee Milk Tea w/ Coffee Jelly', 'Hokkaido Pearl', 
+        'Thai Pearl Milk Tea', 'Taro Pearl Milk Tea', 'Mango Green Milk Tea', 
+        'Golden Retriever', 'Coconut Pearl Milk Black Tea', 'Coconut Pearl Milk Green Tea', 
+        'Classic Black Tea', 'Classic Green Tea', 'Classic Oolong Tea', 
+        'Honey Black Tea', 'Honey Green Tea', 'Honey Oolong Tea'
+    ];
 
+    // 
 function Kiosk() {
+
+    const onSelectItem = (item) => {
+        alert(`You selected: ${item}`);
+        // add to cart or something
+    };
+    const menuList = menuItems.map(item => 
+        <button className="menu-button" onClick={() => onSelectItem(item)}>
+            {item}
+        </button>
+    );
     return (
         <div className="kioskpage">
+            {/* navigation bar for page top, might be removed from kiosk later */}
             <nav className="navbar">
                 <div className="nav-container">
                     <h1 className="logo">MatchaBoba POS</h1>
+                    <h2 className="pageTitle">Menu</h2>
                     <ul className="nav-links">
                         <li><Link to="/login">Login</Link></li>
                         <li><Link to="/kiosk">Customer Kiosk</Link></li>
                         <li><Link to="/menu">Menu Board</Link></li>
                     </ul>
-                    </div>
+                </div>
             </nav>
 
-            <main className="content">
-                <p>testing the page rn</p>
-            </main>
+            <div className="menu-container">
+                <div className="menu-grid">
+                    {/* {menuItems.map((item, index) (
+                    <button
+                        key={index}
+                        className="menu-button"
+                        onClick={() => onSelectItem(item)}
+                    >
+                        {item}
+                    </button>
+                    ))} */}
+                    <ul>{menuList}</ul>
+                </div>
+            </div>
         </div>
     );
-}
+};
 
 export default Kiosk;
